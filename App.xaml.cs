0501fc4d34012b04e0d9a5e9c90a24ab9357ec10
Wu-Haitao <wuhaitao99.st@gmail.com>
2022-11-23 18:36:31 +0800

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace VideoToASCII
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
